4d5d407b2acde6840903658b15dbe1e2ad502bba
sed <sedademirgf@gmail.com>
2021-10-29 14:39:38 +0100

    create color box and color grid components

---
+++ src/ColorBox.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./ColorBox.css";
+class ColorBox extends Component {
+  render() {
+    return <div className="Color-Box"></div>;
+  }
+}
+
+export default ColorBox;
---
+++ src/ColorGrid.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./ColorGrid.css";
+class ColorGrid extends Component {
+  render() {
+    return (
+      <div className="Color-Grid">
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+        <ColorBox />
+      </div>
+    );
+  }
+}
+
+export default ColorGrid;
